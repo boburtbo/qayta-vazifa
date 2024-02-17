80f387b4821bb190107a91da53f8f80baa8e8183
boburbo <bobur20081608@gmail.com>
2024-02-17 19:07:07 +0500

    4.3qayta dars

---
+++ app.js
@@ -0,0 +1,91 @@
+// let yosh = 23;
+// console.log(yosh);
+
+const id = 'AA0'
+console.log(id);
+
+// let firstName = "bobur"
+// console.log(typeof firstName);
+
+// let son1 =6
+// let son8 = 5
+// console.log(son8 + son1);
+
+
+// let a = 5, b = 10;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log( a % b);
+// console.log(a++);
+// console.log(a);
+// console.log(++a);
+// console.log(a);
+// console.log(b--);
+// console.log(b);
+// console.log(--b);
+// console.log(b);
+
+
+// string
+// let ism = 'Bobur';
+//     let message = 'salom' + '!';
+//     console.log(message);
+// function varAndLetScoping() {
+//     var x = 1;
+  
+//     if (true) {
+//       let x = 2;
+//       console.log(x); // will print 2
+//     }
+  
+//     console.log(x); // will print 1
+//   }
+  
+
+// let yosh = 23;
+// console.log(yosh);
+
+const id = 'AA0'
+console.log(id);
+
+// let firstName = "bobur"
+// console.log(typeof firstName);
+
+// let son1 =6
+// let son8 = 5
+// console.log(son8 + son1);
+
+
+// let a = 5, b = 10;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log( a % b);
+// console.log(a++);
+// console.log(a);
+// console.log(++a);
+// console.log(a);
+// console.log(b--);
+// console.log(b);
+// console.log(--b);
+// console.log(b);
+
+
+// string
+// let ism = 'Bobur';
+//     let message = 'salom' + '!';
+//     console.log(message);
+// function varAndLetScoping() {
+//     var x = 1;
+  
+//     if (true) {
+//       let x = 2;
+//       console.log(x); // will print 2
+//     }
+  
+//     console.log(x); // will print 1
+//   }
+  
